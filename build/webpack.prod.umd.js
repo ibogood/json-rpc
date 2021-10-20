81093489c0c4e464a96587428d1db47ab9a257f5
陈波 <chenbo@yxt.com>
2021-10-20 15:16:54 +0800

    chore(package.json): 1.0.2

---
+++ build/webpack.prod.umd.js
@@ -0,0 +1,12 @@
+const baseConfig = require('./webpack.base')
+const path = require('path');
+module.exports = {
+    ...baseConfig,
+    mode: 'production',
+    output: {
+        filename: 'umd.min.js',
+        path: path.resolve(__dirname, '../dist'),
+        library: 'iframe-json-rpc',
+        libraryTarget: 'umd'
+    }
+}
